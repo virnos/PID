68c9869fce52f02dc96d3e4b6e07a626be6c32bb
virnos <47358251@qq.com>
2016-03-25 00:34:13 +0800

    Signed-off-by: virnos <47358251@qq.com>

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	_ "PID/models/cms"
+	//	m "PID/models"
 	_ "PID/routers"
+	"os"
+	"path"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/Unknwon/com"
+)
+
+const (
+	_DB_NAME        = "data/PID.db"
+	_SQLITE3_DRIVER = "sqlite3"
 )
 
 func main() {
-	orm.Debug = true
-	orm.RunSyncdb("default", false, true)
+	registerDB()
 	beego.Run()
 }
+
+func registerDB() {
+	if !com.IsExist(_DB_NAME) {
+		os.MkdirAll(path.Dir(_DB_NAME), os.ModePerm)
+		os.Create(_DB_NAME)
+	}
+	orm.RegisterDriver(_SQLITE3_DRIVER, orm.DRSqlite)
+	orm.RegisterDataBase("default", _SQLITE3_DRIVER, _DB_NAME, 10)
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
+}
+
+//func initDB() {
+//	project := m.Project{}
+//	m.AddProject()
+//}
--- routers/router.go
+++ routers/router.go
@@ -2,12 +2,11 @@ package routers
 
 import (
 	"PID/controllers"
-	cms "PID/controllers/cms"
 
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/cms/device", &cms.ProjectController{})
+	beego.Router("/project", &controllers.ProjectController{})
 }
